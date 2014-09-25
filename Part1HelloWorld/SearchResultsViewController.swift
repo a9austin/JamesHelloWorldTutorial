760cf917afeb0c35f2b3e469263192942410e3dc
Austin Truong <a9austin@gmail.com>
2014-09-24 19:35:22 -0700

    Finished part 4 ~ part 5

--- Part1HelloWorld/SearchResultsViewController.swift
+++ Part1HelloWorld/SearchResultsViewController.swift
@@ -11,8 +11,10 @@ import UIKit
 class SearchResultsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, APIControllerProtocol{
 
     @IBOutlet var appsTableView : UITableView?
+    var imageCache = [String : UIImage]()
     var api = APIController()
     var tableData = []
+    let kCellIdentifier: String = "SearchResultCell"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,27 +27,66 @@ class SearchResultsViewController: UIViewController, UITableViewDataSource, UITa
         // Dispose of any resources that can be recreated.
     }
     
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath){
+        var rowData: NSDictionary = self.tableData[indexPath.row] as NSDictionary
+        
+        var name: String = rowData["trackName"] as String
+        var formattedPrice: String = rowData["formattedPrice"] as String
+        
+        var alert: UIAlertView = UIAlertView()
+        alert.title = name
+        alert.message = formattedPrice
+        alert.addButtonWithTitle("Ok")
+        alert.show()
+    }
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section:    Int) -> Int {
         return tableData.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestCell")
+        let cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier(kCellIdentifier) as UITableViewCell
         
-        let rowData: NSDictionary = self.tableData[indexPath.row] as NSDictionary
+        var rowData: NSDictionary = self.tableData[indexPath.row] as NSDictionary
         
-        cell.textLabel?.text = rowData["trackName"] as? String
+        let cellText: String? = rowData["trackName"] as? String
+        cell.textLabel?.text = cellText
+        cell.imageView?.image = UIImage(named: "Blank52")
         
         // Grab the artwork URL
         let urlString: NSString = rowData["artworkUrl60"] as NSString
-        let imgUrl: NSURL = NSURL(string: urlString)
-        
-        // Download NSData representation of the image URL
-        let imgData: NSData = NSData(contentsOfURL: imgUrl)
-        cell.imageView?.image = UIImage(data: imgData)
-        
         // Get the formatted path price string for display in the subtitle
         let formattedPrice: NSString = rowData["formattedPrice"] as NSString
+        
+        var image = self.imageCache[urlString]
+        
+        if (image == nil){
+            // If image does not exist download it
+            let imgUrl: NSURL = NSURL(string: urlString)
+            let request: NSURLRequest = NSURLRequest(URL: imgUrl)
+            NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), completionHandler: {(response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+                if error == nil{
+                    image = UIImage(data: data)
+                    
+                    self.imageCache[urlString] = image
+                    dispatch_async(dispatch_get_main_queue(), {
+                        if let cellToUpdate = tableView.cellForRowAtIndexPath(indexPath){
+                            cellToUpdate.imageView?.image = image
+                        }
+                    })
+                }
+                else{
+                    println("Error: \(error.localizedDescription)")
+                }
+            })
+        }
+        else{
+            dispatch_async(dispatch_get_main_queue(), {
+                if let cellToUpdate = tableView.cellForRowAtIndexPath(indexPath){
+                    cellToUpdate.imageView?.image = image
+                }
+            })
+        }
         
         cell.detailTextLabel?.text = formattedPrice
         
